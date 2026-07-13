21b94e77e71a0ad005378ba2e82910d054a7fd81
gbalekage <gbalekage21@gmail.com>
2026-07-13 14:57:37 +0200

    repo clean up

--- backend/src/config/db.config.js
+++
@@ -1,8 +0,0 @@
-const { PrismaClient } = require("@prisma/client");
-const { node_env } = require("./env.config");
-
-const prisma = new PrismaClient({
-  log: node_env === "development" ? ["query", "error", "warn"] : ["error"],
-});
-
-module.exports = { prisma };
